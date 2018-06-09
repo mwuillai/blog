806863c7fc3749e33246bc3a43ff94bdc5e4ba08
Mathieu Wuillai <wuillaimathieu78@gmail.com>
2018-06-10 01:09:19 +0200

    Ajout de la data article

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, flash, request, redirect, url_for, ses
 from flask_mysqldb import MySQL
 from wtforms import Form, StringField, TextAreaField, PasswordField, validators
 from passlib.hash import sha256_crypt
-
-from data import Articles
+from functools import wraps
 
 app = Flask(__name__)
 
@@ -30,26 +29,39 @@ def about():
 
 @app.route('/articles')
 def articles():
-    return render_template('articles.html', articles=Articles())
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM articles")
+    articles = cur.fetchall()
+    return render_template('articles.html', articles=articles)
+    cur.close()
 
 
 @app.route('/article/<string:id>/')
 def article(id):
-    return render_template('article.html', id=id)
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM articles WHERE id = %s", (id))
+    article = cur.fetchone()
+    return render_template('article.html', article=article)
+    cur.close()
 
 
-class RegisterForm(Form):
-    name = StringField('Name', [validators.Length(min=1, max=50)])
-    username = StringField('Username', [validators.Length(min=4, max=25)])
-    email = StringField('Email', [validators.Length(min=6, max=50)])
+class registerform(Form):
+    name = StringField('name', [validators.length(min=1, max=50)])
+    username = StringField('username', [validators.length(min=4, max=25)])
+    email = StringField('email', [validators.length(min=6, max=50)])
     password = PasswordField('password', [validators.DataRequired(
-    ), validators.EqualTo('confirm', message='Passwords do not match')])
-    confirm = PasswordField('Confirm Password')
+    ), validators.EqualTo('confirm', message='passwords do not match')])
+    confirm = PasswordField('confirm password')
+
+
+class ArticleForm(Form):
+    title = StringField('title', [validators.length(min=1, max=255)])
+    body = TextAreaField('body')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
-    form = RegisterForm(request.form)
+    form = registerform(request.form)
     if request.method == 'POST' and form.validate():
         name = form.name.data
         email = form.email.data
@@ -74,7 +86,45 @@ def register():
 
     return render_template('register.html', form=form)
 
-# User login
+
+# Check if user logged in
+def is_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Unauthorized, Please login', 'danger')
+            return redirect(url_for('login'))
+    return wrap
+
+
+@app.route('/add_article', methods=['GET', 'POST'])
+@is_logged_in
+def add_article():
+    form = ArticleForm(request.form)
+    if request.method == 'POST' and form.validate():
+        title = form.title.data
+        body = form.body.data
+
+        # create cursor
+
+        cur = mysql.connection.cursor()
+        cur.execute("INSERT INTO articles(title, body) VALUES(%s, %s)", (
+            title, body))
+
+        # Commit to DB
+
+        mysql.connection.commit()
+
+        cur.close()
+
+        flash('Article add successfully', 'success')
+
+        return redirect(url_for('dashboard'))
+
+    return render_template('add_article.html', form=form)
+
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -117,8 +167,16 @@ def login():
     return render_template('login.html')
 
 @app.route('/dashboard')
+@is_logged_in
 def dashboard():
     return render_template('dashboard.html')
+
+# Logout
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash('You are now logged out')
+    return render_template('login.html')
 
 if __name__ == '__main__':
     app.secret_key="secret123"
